330b1af9d3c76845cacec5078dc4f15a282473ac
walternj <43467311+walternj@users.noreply.github.com>
2019-12-27 20:12:12 +0100

    google maps

--- src/Pages/Contacts/Contacts.js
+++ src/Pages/Contacts/Contacts.js
@@ -101,7 +101,7 @@ export default function Contacts() {
           isMarkerShown
           googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
           loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `200px` }} />}
+          containerElement={<div style={{ height: `300px` }} />}
           mapElement={<div style={{ height: `100%` }} />}
         />
       </div>
