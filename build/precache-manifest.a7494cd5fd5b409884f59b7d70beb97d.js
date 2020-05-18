0362eb112fb2b2a80e3fe9a5547f90c5912ce770
Walter Nunes Jr <43467311+walternj@users.noreply.github.com>
2020-05-18 17:27:52 +0200

    Add files via upload
    build files

---
+++ build/precache-manifest.a7494cd5fd5b409884f59b7d70beb97d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798218ab7fba195abe6b4467a5b573a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e02916be4b9be06a10a",
+    "url": "/static/css/2.7ccf4823.chunk.css"
+  },
+  {
+    "revision": "c04a83da9d48ae5f634e",
+    "url": "/static/css/main.eec1df71.chunk.css"
+  },
+  {
+    "revision": "4e02916be4b9be06a10a",
+    "url": "/static/js/2.3dfdd8aa.chunk.js"
+  },
+  {
+    "revision": "c04a83da9d48ae5f634e",
+    "url": "/static/js/main.0b2bbf69.chunk.js"
+  },
+  {
+    "revision": "ee9f6e359cde3b1b2edf",
+    "url": "/static/js/runtime-main.77ee0fbd.js"
+  },
+  {
+    "revision": "b464889840d2a96a5dc30059d9f4ad9a",
+    "url": "/static/media/DancingScript-Regular.b4648898.ttf"
+  },
+  {
+    "revision": "9571ceae61063b81aa078db58854cb19",
+    "url": "/static/media/at-solid.9571ceae.svg"
+  },
+  {
+    "revision": "08515c4f1f810bf85488d8579635377e",
+    "url": "/static/media/comments-solid.08515c4f.svg"
+  },
+  {
+    "revision": "34e73505be53a83b3d32236dab04f124",
+    "url": "/static/media/facebook-square-brands.34e73505.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "10bd4fadc0c1eedf2ee3b2c552a3b052",
+    "url": "/static/media/img_bg_1.10bd4fad.jpg"
+  },
+  {
+    "revision": "71638ac97f7041072d9e140d7904c84a",
+    "url": "/static/media/img_bg_2.71638ac9.jpg"
+  },
+  {
+    "revision": "3f716ea81798c1a3b175442306ed2220",
+    "url": "/static/media/img_bg_3.3f716ea8.jpg"
+  },
+  {
+    "revision": "e75317fafc5e835195adf92be513d35b",
+    "url": "/static/media/instagram-brands.e75317fa.svg"
+  },
+  {
+    "revision": "d9049f461eb3dd661b7f77c6e56e7593",
+    "url": "/static/media/linkedin-brands.d9049f46.svg"
+  },
+  {
+    "revision": "9ca393d53c6a57e870721e9fe95f708f",
+    "url": "/static/media/menuBackground.9ca393d5.jpg"
+  },
+  {
+    "revision": "bacc2d0a2820b3626f60e35deada6d0c",
+    "url": "/static/media/phone-alt-solid.bacc2d0a.svg"
+  },
+  {
+    "revision": "c80ca203c0f773a8502d85ae60f347ce",
+    "url": "/static/media/youtube-brands.c80ca203.svg"
+  }
+]);
